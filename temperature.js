4777a8a3c48de946b00fadbad4cf83bc8e69d812
Pedro Javier <pedro.javier.nunez.rodriguez@gmail.com>
2013-02-13 09:51:16 +0000

    version 1.2

--- temperature.js
+++ temperature.js
@@ -6,22 +6,23 @@ function calculate() {
   var regexp = /-?[0-9]*\.?[0-9]*[a-zA-Z]+/;
   
   var m = temp.match(regexp);
+  m = m[0];
   
   if (m) {
     var num = m.substring(m.length-1, 0);
     var type = m.substring(1, m.length-1);
     num = parseFloat(num);
     if (type == 'c' || type == 'C') {
-      result = (num * 9/5)+32;
-      result = result + 'C';
+       result = (num * (9/5))+32;
+       result = result + 'C';
     }
     else {
-      result = (num - 32)*5/9;
-      result = result + 'F';
+       result = (num - 32)*(5/9);
+       result = result + 'F';
     }
     converted.innerHTML = result;
   }
   else {
-    converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
+     converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
   }
 }
